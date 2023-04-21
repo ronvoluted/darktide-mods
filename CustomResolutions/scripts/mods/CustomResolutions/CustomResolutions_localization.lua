5d4bcddfc2a8b94e19224f420f374eb667a1661b
deluxghost <deluxghost@users.noreply.github.com>
2023-04-21 18:36:19 +0800

    add some zh-cn localization

--- CustomResolutions/scripts/mods/CustomResolutions/CustomResolutions_localization.lua
+++ CustomResolutions/scripts/mods/CustomResolutions/CustomResolutions_localization.lua
@@ -1,20 +1,26 @@
 return {
 	mod_name = {
 		en = "Custom Resolutions",
+		["zh-cn"] = "自定义分辨率",
 	},
 	mod_description = {
 		en = "Add custom resolutions to the list of video options",
+		["zh-cn"] = "在视频选项内添加自定义分辨率",
 	},
 	append_position = {
 		en = "Placement of new options",
+		["zh-cn"] = "新选项的位置",
 	},
 	append_position_tooltip = {
 		en = "(Will apply after restart)",
+		["zh-cn"] = "（重新启动后生效）",
 	},
 	append_position_start = {
 		en = "Add to start of dropdown list",
+		["zh-cn"] = "在下拉框的顶部",
 	},
 	append_position_end = {
 		en = "Add to end of dropdown list",
+		["zh-cn"] = "在下拉框的底部",
 	},
 }
--- PrivateMissionStatus/scripts/mods/PrivateMissionStatus/PrivateMissionStatus_localization.lua
+++ PrivateMissionStatus/scripts/mods/PrivateMissionStatus/PrivateMissionStatus_localization.lua
@@ -1,14 +1,18 @@
 return {
 	mod_name = {
 		en = "Private Mission Status",
+		["zh-cn"] = "私人任务状态",
 	},
 	mod_description = {
 		en = "Indicate in the tactical overlay whether the current mission is public or private.",
+		["zh-cn"] = "在战术覆盖内显示当前任务是公共还是私人。",
 	},
 	public = {
 		en = "PUBLIC",
+		["zh-cn"] = "公共",
 	},
 	private = {
 		en = "PRIVATE",
+		["zh-cn"] = "私人",
 	},
 }
