a01709ecb4b89a58f3958015cd0456435c9b4c4b
deluxghost <deluxghost@users.noreply.github.com>
2023-04-21 18:36:19 +0800

    add some zh-cn localization

--- ForTheEmperor/scripts/mods/ForTheEmperor/ForTheEmperor_localization.lua
+++ ForTheEmperor/scripts/mods/ForTheEmperor/ForTheEmperor_localization.lua
@@ -1,17 +1,22 @@
 return {
 	mod_title = {
 		en = "For the Emperor!",
+		["zh-cn"] = "为了帝皇！",
 	},
 	mod_description = {
 		en = "Shout \"For the Emperor!\", \"For Atoma!\" or words of camaraderie from the comms wheel.",
+		["zh-cn"] = "通过标签轮盘喊出“为了帝皇！”、“为了亚托玛！”或其他友谊之词。",
 	},
 	wheel_position = {
 		en = "Communication wheel position",
+		["zh-cn"] = "标签轮盘位置",
 	},
 	wheel_left = {
 		en = "Left wheel option",
+		["zh-cn"] = "左侧轮盘选项",
 	},
 	wheel_right = {
 		en = "Right wheel option",
+		["zh-cn"] = "右侧轮盘选项",
 	},
 }
