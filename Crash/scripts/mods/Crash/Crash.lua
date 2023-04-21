78a5280f6cb9a8e0efcc003ff3eeabbc94a8ab54
deluxghost <deluxghost@users.noreply.github.com>
2023-04-21 19:39:13 +0800

    add crash localization

--- Crash/scripts/mods/Crash/Crash.lua
+++ Crash/scripts/mods/Crash/Crash.lua
@@ -21,16 +21,20 @@ mod.update = function (dt)
 	if last_message_time == nil and os.clock() - start_time + dt >= 1 + (delay % 1) or
 		type(last_message_time) == "number" and os.clock() - last_message_time + dt >= 1 then
 		local seconds_remaining = math.floor(crash_time - os.clock())
-		mod:echo("Crashing in " .. seconds_remaining .. (seconds_remaining == 1 and " second..." or " seconds..."))
+		if seconds_remaining == 1 then
+			mod:echo(mod:localize("message_crashing_in_second", tostring(seconds_remaining)))
+		else
+			mod:echo(mod:localize("message_crashing_in_seconds", tostring(seconds_remaining)))
+		end
 		last_message_time = os.clock()
 	end
 end
 
-mod:command("crash", "Crash the game immediately, or specify seconds to schedule", function(delay_seconds)
+mod:command("crash", mod:localize("command_crash_desc"), function(delay_seconds)
 	delay = tonumber(delay_seconds)
 
 	if type(delay) == "number" then
-		mod:echo("Crashing in " .. string.format("%g", string.format("%.3f", delay)) .. " seconds...")
+		mod:echo(mod:localize("message_crashing_in_seconds", string.format("%g", string.format("%.3f", delay))))
 		start_time = os.clock()
 		crash_time = os.clock() + delay
 	else
@@ -38,26 +42,26 @@ mod:command("crash", "Crash the game immediately, or specify seconds to schedule
 	end
 end)
 
-mod:command("crash_cancel", "Cancel a scheduled crash", function()
+mod:command("crash_cancel", mod:localize("command_crash_cancel_desc"), function()
 	if crash_time ~= nil then
 		delay, start_time, crash_time, last_message_time = nil, nil, nil, nil
-		mod:echo("Crash cancelled")
+		mod:echo(mod:localize("message_cancelled"))
 	else
-		mod:echo("No crash scheduled")
+		mod:echo(mod:localize("message_not_scheduled"))
 	end
 end)
 
-mod:command("error", "Create an error from message", function(...)
+mod:command("error", mod:localize("command_error_desc"), function(...)
 	local message = table.concat({...}, " ")
 
 	if message == "" then
-			error("Error created")
+			error(mod:localize("message_default_error"))
 			return
 	else
 		error(message)
 	end
 end)
 
-mod:command("exit", "Exit the game immediately", function()
+mod:command("exit", mod:localize("command_exit_desc"), function()
 	Application.quit()
 end)
