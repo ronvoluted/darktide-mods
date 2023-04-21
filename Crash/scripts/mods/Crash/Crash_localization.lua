5adba2ff6ae0db159c7ee123484c575b553c1272
deluxghost <deluxghost@users.noreply.github.com>
2023-04-21 19:39:13 +0800

    add crash localization

--- Crash/scripts/mods/Crash/Crash_localization.lua
+++ Crash/scripts/mods/Crash/Crash_localization.lua
@@ -1,5 +1,46 @@
 return {
+	mod_name = {
+		en = "Crash",
+		["zh-cn"] = "手动崩溃",
+	},
 	mod_description = {
-		en = "Provides several commands to send your game into the immaterium :)"
+		en = "Provides several commands to send your game into the immaterium :)",
+		["zh-cn"] = "提供一些命令把你的游戏送入亚空间 :)",
+	},
+	command_crash_desc = {
+		en = "Crash the game immediately, or specify seconds to schedule",
+		["zh-cn"] = "立即让游戏崩溃，或计划指定秒数后崩溃",
+	},
+	command_crash_cancel_desc = {
+		en = "Cancel a scheduled crash",
+		["zh-cn"] = "取消已计划的崩溃",
+	},
+	command_error_desc = {
+		en = "Create an error from message",
+		["zh-cn"] = "根据消息创建错误",
+	},
+	command_exit_desc = {
+		en = "Exit the game immediately",
+		["zh-cn"] = "立即退出游戏",
+	},
+	message_crashing_in_second = {
+		en = "Crashing in %s second...",
+		["zh-cn"] = "将在 %s 秒后崩溃...",
+	},
+	message_crashing_in_seconds = {
+		en = "Crashing in %s seconds...",
+		["zh-cn"] = "将在 %s 秒后崩溃...",
+	},
+	message_cancelled = {
+		en = "Crash cancelled",
+		["zh-cn"] = "崩溃已取消",
+	},
+	message_not_scheduled = {
+		en = "No crash scheduled",
+		["zh-cn"] = "没有已计划的崩溃",
+	},
+	message_default_error = {
+		en = "Error created",
+		["zh-cn"] = "错误已创建",
 	},
 }
