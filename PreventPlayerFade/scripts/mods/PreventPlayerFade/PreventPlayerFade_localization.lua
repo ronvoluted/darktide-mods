7ae4f37eb084c68dda34c773bb71daaadd4817ad
deluxghost <deluxghost@users.noreply.github.com>
2023-05-15 17:04:56 +0800

    Update PreventPlayerFade_localization.lua

--- PreventPlayerFade/scripts/mods/PreventPlayerFade/PreventPlayerFade_localization.lua
+++ PreventPlayerFade/scripts/mods/PreventPlayerFade/PreventPlayerFade_localization.lua
@@ -1,8 +1,10 @@
 return {
 	mod_name = {
 		en = "Prevent Player Fade",
+		["zh-cn"] = "防止玩家透明",
 	},
 	mod_description = {
 		en = "Prevent the player model from fading out when the camera's view is close.",
+		["zh-cn"] = "视角过近时，防止玩家模型变透明。",
 	},
 }
