2a8ee810ba0f52da0d0939dd5817fb731bd79349
deluxghost <deluxghost@gmail.com>
2023-06-07 01:16:27 +0800

    add will of the emperor zh-cn i18n

--- WillOfTheEmperor/scripts/mods/WillOfTheEmperor/WillOfTheEmperor_localization.lua
+++ WillOfTheEmperor/scripts/mods/WillOfTheEmperor/WillOfTheEmperor_localization.lua
@@ -1,158 +1,208 @@
 return {
 	mod_name = {
 		["en"] = "Will of The Emperor",
+		["zh-cn"] = "帝皇的意志（单人模式增强）",
 	},
 	mod_description = {
 		["en"] = "Enact The Emperor's will in clientside maps (Psykhanium, Prologue, Solo Play) and enable a cornucopia of bestowments.\n\nAuthor: Seventeen",
+		["zh-cn"] = "在本地地图中（灵能室、序章、单人任务）执行帝皇的意志，并启用一系列帝皇赐福。\n\n作者：Seventeen",
 	},
 	god_emperor_mode = {
 		["en"] = "God-Emperor mode",
+		["zh-cn"] = "神皇模式",
 	},
 	god_emperor_mode_desc = {
 		["en"] = "Prevent all damage. The God-Emperor protects.",
+		["zh-cn"] = "不受任何伤害。神皇保佑。",
 	},
 	fervency_mode = {
 		["en"] = "Fervency mode",
+		["zh-cn"] = "狂热模式",
 	},
 	fervency_mode_desc = {
 		["en"] = "Survive ten wounds and stand up on your own. Shed blood ceaslessly in honour of The Emperor.",
+		["zh-cn"] = "随身携带十个生命格并自主站起。为帝皇的荣誉流尽鲜血。",
 	},
 	disable_enemy_spawns = {
 		["en"] = "Disable enemy spawns",
+		["zh-cn"] = "禁用敌人生成",
 	},
 	disable_enemy_spawns_desc = {
 		["en"] = "Explore maps at your leisure without any enemies. Glory to The Emperor.",
+		["zh-cn"] = "在没有异端的情况下肆意探索地图。荣耀属于帝皇。",
 	},
 	invisibility = {
 		["en"] = "Invisibility",
+		["zh-cn"] = "隐身",
 	},
 	invisibility_desc = {
 		["en"] = "Pass by enemies undetected. Only the righteous preceive The Emperor's majesty.",
+		["zh-cn"] = "不被敌人发现。正义者才能一睹帝皇的威严。",
 	},
 	rapid_ability_cooldowns = {
 		["en"] = "Rapid ability cooldown",
+		["zh-cn"] = "快速技能冷却",
 	},
 	rapid_ability_cooldowns_desc = {
 		["en"] = "Use your ability feat whenever there are heretics to smite. Always. The Emperor's might is unending.",
+		["zh-cn"] = "只要异端尚未荡平，就能发动你的技能。帝皇的威能无穷无尽。",
 	},
 	infinite_slide = {
 		["en"] = "Infinite slide",
+		["zh-cn"] = "无限滑铲",
 	},
 	infinite_slide_desc = {
 		["en"] = "Keep holding crouch to slide, even after animations end. The Emperor persists.",
+		["zh-cn"] = "按住蹲伏键滑铲，即使动作结束也可以继续。帝皇在坚持。",
 	},
 	infinite_ammunition = {
 		["en"] = "Infinite ammunition",
+		["zh-cn"] = "无限弹药",
 	},
 	infinite_ammunition_desc = {
 		["en"] = "Unlimited rounds/magazines/energy/fuel. The Emperor provides.",
+		["zh-cn"] = "无限子弹、弹匣、能量、燃料。帝皇在赐予。",
 	},
 	no_reloading = {
 		["en"] = "No reloading",
+		["zh-cn"] = "无需装填",
 	},
 	no_reloading_desc = {
 		["en"] = "Keep firing without needing to reload. The Emperor's will is absolute.",
+		["zh-cn"] = "连续开火，无需装填。帝皇的意志是绝对的。",
 	},
 	rounds_in_chamber = {
 		["en"] = "Rounds in chamber",
+		["zh-cn"] = "枪膛中的弹药数",
 	},
 	rounds_in_chamber_desc = {
 		["en"] = "If No Reloads is set, deplete rounds to this amount. Trust in The Emperor.",
+		["zh-cn"] = "如果启用无需装填，则将弹药消耗到此数量。相信帝皇。",
 	},
 	rounds_1 = {
 		["en"] = "1 remaining",
+		["zh-cn"] = "剩余 1 发",
 	},
 	rounds_2 = {
 		["en"] = "2 remaining",
+		["zh-cn"] = "剩余 2 发",
 	},
 	rounds_3 = {
 		["en"] = "3 remaining",
+		["zh-cn"] = "剩余 3 发",
 	},
 	rounds_4 = {
 		["en"] = "4 remaining",
+		["zh-cn"] = "剩余 4 发",
 	},
 	rounds_5 = {
 		["en"] = "5 remaining",
+		["zh-cn"] = "剩余 5 发",
 	},
 	rounds_minus_0 = {
 		["en"] = "1st shot/full",
+		["zh-cn"] = "第 1 发 / 满弹匣",
 	},
 	rounds_minus_1 = {
 		["en"] = "2nd shot",
+		["zh-cn"] = "第 2 发",
 	},
 	rounds_minus_2 = {
 		["en"] = "3rd shot",
+		["zh-cn"] = "第 3 发",
 	},
 	rounds_minus_3 = {
 		["en"] = "4th shot",
+		["zh-cn"] = "第 4 发",
 	},
 	rounds_minus_4 = {
 		["en"] = "5th shot",
+		["zh-cn"] = "第 5 发",
 	},
 	-- infinite_coherency = {
 	-- 	["en"] = "Infinite coherency",
+	-- 	["zh-cn"] = "无限连携",
 	-- },
 	-- infinite_coherency_desc = {
 	-- 	["en"] = "",
 	-- },
 	infinite_ledge_hold = {
 		["en"] = "Infinite ledge hold",
+		["zh-cn"] = "无限悬挂",
 	},
 	infinite_ledge_hold_desc = {
 		["en"] = "Never give up when hanging from a fall. The Emperor's hold is eternal.",
+		["zh-cn"] = "跌落悬崖时永不放弃。帝皇的掌控是永恒的。",
 	},
 	-- no_fall_damage = {
 	-- 	["en"] = "No fall damage",
+	-- 	["zh-cn"] = "无掉落伤害",
 	-- },
 	-- no_fall_damage_desc = {
 	-- 	["en"] = "",
 	-- },
 	move_speed = {
 		["en"] = "Move speed",
+		["zh-cn"] = "移动速度",
 	},
 	move_speed_desc = {
 		["en"] = "Adjust walk speed. The Emperor's mission waits for none.",
+		["zh-cn"] = "调整移动速度。帝皇的使命不等人。",
 	},
 	-- attack_speed = {
 	-- 	["en"] = "Attack speed",
+	-- 	["zh-cn"] = "攻击速度",
 	-- },
 	-- attack_speed_desc = {
 	-- 	["en"] = "\"Smack em good\" - John Emperor",
+	-- 	["zh-cn"] = "“打他们就对了” - 约翰帝皇",
 	-- },
 	gravity = {
 		["en"] = "Gravity",
+		["zh-cn"] = "重力",
 	},
 	gravity_desc = {
 		["en"] = "Modify the forces of gravity. The Emperor's truth is irrefutable.",
+		["zh-cn"] = "修改重力。帝皇的真理无可辩驳。",
 	},
 	reset_sliders_on_reload = {
 		["en"] = "Reset sliders on reload",
+		["zh-cn"] = "重新加载时重置滑块",
 	},
 	reset_sliders_on_reload_desc = {
 		["en"] = "Set move speed and gravity to default when the game restarts or mods reload. Uphold unwavering faith in The Emperor.",
+		["zh-cn"] = "重启游戏或重新加载模组时设置移动速度和重力为默认值。坚持对皇帝的坚定信念。",
 	},
 	toggle_pause_time = {
 		["en"] = "Pause/Resume time",
+		["zh-cn"] = "暂停 / 恢复时间",
 	},
 	toggle_pause_time_desc = {
 		["en"] = "Stop and continue the speed of time itself. The Emperor's will is infinite.",
+		["zh-cn"] = "停止和恢复时间的流动。帝皇的意志是无限的。",
 	},
 	slow_mo = {
 		["en"] = "Slow-mo",
+		["zh-cn"] = "慢动作",
 	},
 	slow_mo_desc = {
 		["en"] = "Enter/exit slow-mo. Millenia, has Him on Terra endured.",
+		["zh-cn"] = "进入 / 退出慢动作。千禧年，他在泰拉经受了考验。",
 	},
 	reset_time = {
 		["en"] = "Reset time",
+		["zh-cn"] = "重置时间",
 	},
 	reset_time_desc = {
 		["en"] = "Set time speed to normal. The Immortal Emperor will restore the Imperium of Man.",
+		["zh-cn"] = "恢复正常时间速度。不朽的帝皇将恢复人类的帝国。",
 	},
 	-- air_move_speed_scale = {
 	-- 	["en"] = "Airborne move speed",
+	-- 	["zh-cn"] = "滞空时移动速度",
 	-- },
 	-- air_move_speed_scale_desc = {
 	-- 	["en"] = "Amount of movement control while mid-air. By The Emperor's Faith.",
+	-- 	["zh-cn"] = "在半空中的移动速度控制。以皇帝的信仰。",
 	-- },
 }
