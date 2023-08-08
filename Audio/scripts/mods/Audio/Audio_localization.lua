6a1e06095087a1ad8839b241bcf6bd912761b416
deluxghost <deluxghost@users.noreply.github.com>
2023-08-08 22:49:31 +0800

    update zh-cn text

--- Audio/scripts/mods/Audio/Audio_localization.lua
+++ Audio/scripts/mods/Audio/Audio_localization.lua
@@ -1,8 +1,10 @@
 return {
 	mod_name = {
 		["en"] = "Audio Plugin",
+		["zh-cn"] = "音频插件",
 	},
 	mod_description = {
 		["en"] = "Library for playing custom sound files and managing WWise events.\n\nAuthor: Seventeen ducks in a trenchcoat",
+		["zh-cn"] = "用于播放自定义音频文件并管理 WWise 事件的公共库。\n\n作者：Seventeen ducks in a trenchcoat",
 	},
 }
