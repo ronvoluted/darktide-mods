9da18b604a005827efd84e2e2c8ba7a404f0e06e
xsSplater <xssplater@gmail.com>
2023-10-09 04:31:33 +0300

    RU DarktideLocalServer_localization.lua

--- DarktideLocalServer/scripts/mods/DarktideLocalServer/DarktideLocalServer_localization.lua
+++ DarktideLocalServer/scripts/mods/DarktideLocalServer/DarktideLocalServer_localization.lua
@@ -1,8 +1,10 @@
 return {
 	mod_name = {
 		en = "Darktide Local Server",
+		ru = "Локальный сервер Darktide",
 	},
 	mod_description = {
 		en = "Enable enhanced functionality in other mods. Developer Mode and Show Developer Console must be On in Darktide Mod Framework options.\n\nAuthor: Seventeen ducks in a trenchcoat",
+		ru = "Darktide Local Server - Включите расширенную функциональность в других модах. В настройках Darktide Mod Framework должны быть включены «Режим разработчика» и «Показать консоль разработчика».\nАвтор: Seventeen ducks in a trenchcoat",
 	},
 }
