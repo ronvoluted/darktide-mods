7861e583efa4b40509e12f51142167ac947f47a6
Ron Au <5785323+ronvoluted@users.noreply.github.com>
2023-10-15 17:15:20 +1100

    feat: Add C-Thru Shields

---
+++ CThruShields/scripts/mods/CThruShields/CThruShields_localization.lua
@@ -0,0 +1,8 @@
+return {
+	mod_name = {
+		en = "C-Thru Shields",
+	},
+	mod_description = {
+		en = "Makes your slab shield transparent when bracing.\n\nAuthor: Seventeen ducks in a trenchcoat",
+	},
+}
