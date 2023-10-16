2f9a966b0985b467c8280339e5810509367da67e
xsSplater <xssplater@gmail.com>
2023-10-16 16:17:36 +0300

    RU CThruShields localization

--- CThruShields/scripts/mods/CThruShields/CThruShields_localization.lua
+++ CThruShields/scripts/mods/CThruShields/CThruShields_localization.lua
@@ -1,8 +1,10 @@
 return {
 	mod_name = {
 		en = "C-Thru Shields",
+		ru = "Смотри через щит",
 	},
 	mod_description = {
 		en = "Makes your slab shield transparent when bracing.\n\nAuthor: Seventeen ducks in a trenchcoat",
+		ru = "C-Thru Shields - Делает щит-плиту прозрачным при переноске.\n\nАвтор: Seventeen ducks in a trenchcoat",
 	},
 }
