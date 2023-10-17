2cea498dc88af805f01a50999c8f460a53ef9f61
Ron Au <5785323+ronvoluted@users.noreply.github.com>
2023-10-17 13:16:27 +1100

    refactor(ElevatorMusic): Update English

--- ElevatorMusic/scripts/mods/ElevatorMusic/ElevatorMusic_localization.lua
+++ ElevatorMusic/scripts/mods/ElevatorMusic/ElevatorMusic_localization.lua
@@ -1,22 +1,22 @@
 return {
 	mod_name = {
-		en = "Elevator Music",
+		["en"] = "Elevator Music",
 		["zh-cn"] = "电梯音乐",
 	},
 	mod_description = {
-		en = "In the grim darkness of the far future— somehow, muzak has returned. Developer Mode and Show Developer Console must be On in Darktide Mod Framework options.\n\nAuthor: Seventeen ducks in a trenchcoat",
+		["en"] = "In the grim darkness of the far future— somehow, muzak has returned. Developer Mode and Show Developer Console must be On in Darktide Mod Framework options.\n\nAuthor: Seventeen ducks in a trenchcoat",
 		["zh-cn"] = "在遥远未来的严酷黑暗中——不知何故，电梯音乐（穆扎克）归来了。Darktide Mod Framework 选项中必须启用“开发者模式”和“显示开发者控制台”。\n\n作者：Seventeen ducks in a trenchcoat",
 	},
 	muzak_volume = {
-		en = "Muzak volume",
+		["en"] = "Muzak volume",
 		["zh-cn"] = "电梯音乐音量",
 	},
-	ascension_riser = {
-		en = "Ascension Riser",
+	enable_for_ascension_riser = {
+		["en"] = "Ascension Riser",
 		["zh-cn"] = "大型升降机",
 	},
-	ascension_riser_tooltip = {
-		en = "Play Muzak while riding the Ascension Riser. It's a glorified lift, right?",
+	enable_for_ascension_riser_tooltip = {
+		["en"] = "Play Muzak while riding the Ascension Riser. It's a glorified lift, right?",
 		["zh-cn"] = "搭乘大型升降机时也播放电梯音乐。只不过是一种更神圣的电梯，不是吗？",
 	},
 }
