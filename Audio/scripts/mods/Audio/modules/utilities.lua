5b5bb80ca379bf8601f24981bb19910ec7b288ec
Ron Au <5785323+ronvoluted@users.noreply.github.com>
2023-10-21 16:27:43 +1100

    feat(Audio): Add get_delay_seconds utility

--- Audio/scripts/mods/Audio/modules/utilities.lua
+++ Audio/scripts/mods/Audio/modules/utilities.lua
@@ -1,20 +1,43 @@
-local Audio = get_mod("Audio")
+---Extract the minimum delay from an FFplay `adelay` string
+---@param adelay_string string The `adelay` value used in `playback_settings`
+---@return number minDelay The shortest delay in seconds, or 0 if there are none
+local get_delay_seconds = function(adelay_string)
+	local withoutColon = string.match(adelay_string, "([^:]+)")
+	local minDelay = 0
+	local delays = {}
 
----Distinguish if userdata is a position or unit
----@param userdata userdata A userdata value
----@return "Unit"|"Vector3"|nil type Type of the userdata
-Audio.userdata_type = function(userdata)
-	if type(userdata) ~= "userdata" then
-		return nil
+	if not withoutColon then
+		return minDelay
 	end
 
-	if Unit.alive(userdata) then
-		return "Unit"
-	elseif Vector3.is_valid(userdata) then
-		return "Vector3"
-	else
-		return tostring(userdata)
+	for delay in string.gmatch(withoutColon, "([^|]+)") do
+		table.insert(delays, delay)
 	end
+
+	for _, delay in pairs(delays) do
+		local number, suffix = string.match(delay, "([%d%.]+)([sS]?)")
+		if number then
+			number = tonumber(number)
+
+			if type(number) == "number" then
+				local delaySeconds
+
+				if suffix == "S" then
+					delaySeconds = number / 48000 -- I am okay with this
+				elseif suffix == "s" then
+					delaySeconds = number
+				else
+					delaySeconds = number / 1000
+				end
+
+				if minDelay == 0 or delaySeconds < minDelay then
+					minDelay = delaySeconds
+				end
+			end
+		end
+	end
+
+	return minDelay
 end
 
 ---Determine the mod that is calling the current scope
@@ -53,7 +76,7 @@ end
 ---Distinguish if userdata is a position or unit
 ---@param userdata userdata A userdata value
 ---@return "Unit"|"Vector3"|nil type Type of the userdata
-local userdata_type = function(userdata)
+local get_userdata_type = function(userdata)
 	if type(userdata) ~= "userdata" then
 		return nil
 	end
@@ -68,7 +91,7 @@ local userdata_type = function(userdata)
 end
 
 return {
-	getDelay = getDelay,
+	get_delay_seconds = get_delay_seconds,
 	function_caller_mod_name = function_caller_mod_name,
-	userdata_type = userdata_type,
+	get_userdata_type = get_userdata_type,
 }
--- Audio/scripts/mods/Audio/tests/utilities.test.lua
+++ Audio/scripts/mods/Audio/tests/utilities.test.lua
@@ -1,9 +1,32 @@
 local Audio = get_mod("Audio")
+local utilities = Audio:io_dofile("Audio/scripts/mods/Audio/modules/utilities")
+local function_caller_mod_name = utilities.function_caller_mod_name
+local get_delay_seconds = utilities.get_delay_seconds
+local get_userdata_type = utilities.get_userdata_type
+
+local caller_mod = {
+	description = "returns the name of the current mod",
+	fun = function()
+		return function_caller_mod_name()
+	end,
+	expected = "Tests",
+}
+
+local get_delay = {
+	description = "extract mininum delay from `adelay`",
+	fun = function()
+		assert(get_delay_seconds("") == 0)
+		assert(get_delay_seconds("500|780") == 0.5)
+		assert(get_delay_seconds("5s|2.3s:all=1") == 2.3)
+		assert(get_delay_seconds("96000S|888888S:all=1") == 2)
+		assert(get_delay_seconds("6784|12s|480000S|9000:all=1") == 6.784)
+	end,
+}
 
 local userdata_vector3 = {
 	description = "returns userdata type for Vector3",
 	fun = function()
-		return Audio.userdata_type(Vector3.one())
+		return get_userdata_type(Vector3.one())
 	end,
 	expected = "Vector3",
 }
@@ -12,24 +35,17 @@ local userdata_unit = {
 	description = "returns userdata type for Unit",
 	fun = function()
 		local player_unit = Managers.player:local_player_safe(1).player_unit
-		return Audio.userdata_type(player_unit)
+		return get_userdata_type(player_unit)
 	end,
 	expected = "Unit",
-}
-
-local caller_mod = {
-	description = "returns the name of the current mod",
-	fun = function()
-		return Audio.function_caller_mod_name()
-	end,
-	expected = "Tests",
 }
 
 Audio._tests.suites["utilities"] = {
 	description = "Utilities",
 	tests = {
+		caller_mod,
+		get_delay,
 		userdata_vector3,
 		userdata_unit,
-		caller_mod,
 	},
 }
